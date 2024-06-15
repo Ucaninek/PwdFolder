392a3ef56069a30239d268a129ec0c219f58db18
Zemi <43094930+Ucaninek@users.noreply.github.com>
2024-06-15 16:59:10 +0300

    feat: ➕ Add buttons and labels

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,19 @@
         private void InitializeComponent()
         {
             L_Greet = new Label();
-            P_ProtectFolder = new Panel();
-            TB_Password = new MaskedTextBox();
-            B_ProtectFolder = new Button();
             L_MiddleText = new Label();
-            P_ProtectFolder.SuspendLayout();
+            TB_Password = new MaskedTextBox();
+            P_Controls = new Panel();
+            TC_Buttons = new TabControl();
+            TP_UnprotectedFolder = new TabPage();
+            B_ProtectFolder = new Button();
+            TP_ProtectedFolder = new TabPage();
+            button2 = new Button();
+            button1 = new Button();
+            P_Controls.SuspendLayout();
+            TC_Buttons.SuspendLayout();
+            TP_UnprotectedFolder.SuspendLayout();
+            TP_ProtectedFolder.SuspendLayout();
             SuspendLayout();
             // 
             // L_Greet
@@ -42,78 +50,142 @@
             L_Greet.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             L_Greet.Location = new Point(0, 0);
             L_Greet.Name = "L_Greet";
-            L_Greet.Size = new Size(324, 35);
+            L_Greet.Size = new Size(324, 42);
             L_Greet.TabIndex = 0;
             L_Greet.Text = "Helo";
-            L_Greet.TextAlign = ContentAlignment.TopCenter;
+            L_Greet.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // P_ProtectFolder
+            // L_MiddleText
             // 
-            P_ProtectFolder.Controls.Add(TB_Password);
-            P_ProtectFolder.Controls.Add(B_ProtectFolder);
-            P_ProtectFolder.Dock = DockStyle.Bottom;
-            P_ProtectFolder.Location = new Point(0, 260);
-            P_ProtectFolder.Margin = new Padding(6);
-            P_ProtectFolder.Name = "P_ProtectFolder";
-            P_ProtectFolder.Padding = new Padding(3);
-            P_ProtectFolder.Size = new Size(324, 76);
-            P_ProtectFolder.TabIndex = 1;
+            L_MiddleText.AutoEllipsis = true;
+            L_MiddleText.Dock = DockStyle.Top;
+            L_MiddleText.Font = new Font("Segoe UI", 12F);
+            L_MiddleText.ForeColor = SystemColors.ControlDarkDark;
+            L_MiddleText.Location = new Point(0, 42);
+            L_MiddleText.Name = "L_MiddleText";
+            L_MiddleText.Size = new Size(324, 295);
+            L_MiddleText.TabIndex = 2;
+            L_MiddleText.Text = "label1";
+            L_MiddleText.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // TB_Password
             // 
-            TB_Password.Dock = DockStyle.Top;
             TB_Password.Font = new Font("Segoe UI", 12F);
-            TB_Password.Location = new Point(3, 3);
+            TB_Password.Location = new Point(10, 6);
             TB_Password.Name = "TB_Password";
             TB_Password.PasswordChar = '•';
-            TB_Password.Size = new Size(318, 29);
+            TB_Password.Size = new Size(304, 29);
             TB_Password.TabIndex = 1;
+            // 
+            // P_Controls
+            // 
+            P_Controls.Controls.Add(TB_Password);
+            P_Controls.Controls.Add(TC_Buttons);
+            P_Controls.Dock = DockStyle.Bottom;
+            P_Controls.Location = new Point(0, 216);
+            P_Controls.Margin = new Padding(6);
+            P_Controls.Name = "P_Controls";
+            P_Controls.Padding = new Padding(3);
+            P_Controls.Size = new Size(324, 120);
+            P_Controls.TabIndex = 1;
+            P_Controls.Visible = false;
+            // 
+            // TC_Buttons
+            // 
+            TC_Buttons.Controls.Add(TP_UnprotectedFolder);
+            TC_Buttons.Controls.Add(TP_ProtectedFolder);
+            TC_Buttons.Location = new Point(3, 41);
+            TC_Buttons.Name = "TC_Buttons";
+            TC_Buttons.SelectedIndex = 0;
+            TC_Buttons.Size = new Size(318, 77);
+            TC_Buttons.TabIndex = 3;
+            // 
+            // TP_UnprotectedFolder
+            // 
+            TP_UnprotectedFolder.Controls.Add(B_ProtectFolder);
+            TP_UnprotectedFolder.Location = new Point(4, 24);
+            TP_UnprotectedFolder.Name = "TP_UnprotectedFolder";
+            TP_UnprotectedFolder.Padding = new Padding(3);
+            TP_UnprotectedFolder.Size = new Size(310, 49);
+            TP_UnprotectedFolder.TabIndex = 0;
+            TP_UnprotectedFolder.Text = "Protect";
+            TP_UnprotectedFolder.UseVisualStyleBackColor = true;
             // 
             // B_ProtectFolder
             // 
             B_ProtectFolder.Dock = DockStyle.Bottom;
             B_ProtectFolder.Font = new Font("Segoe UI", 12F);
-            B_ProtectFolder.Location = new Point(3, 34);
+            B_ProtectFolder.Location = new Point(3, 7);
             B_ProtectFolder.Name = "B_ProtectFolder";
-            B_ProtectFolder.Size = new Size(318, 39);
-            B_ProtectFolder.TabIndex = 0;
+            B_ProtectFolder.Size = new Size(304, 39);
+            B_ProtectFolder.TabIndex = 1;
             B_ProtectFolder.Text = "Protect Folder";
             B_ProtectFolder.UseVisualStyleBackColor = true;
             // 
-            // L_MiddleText
+            // TP_ProtectedFolder
             // 
-            L_MiddleText.AutoEllipsis = true;
-            L_MiddleText.Dock = DockStyle.Fill;
-            L_MiddleText.Font = new Font("Segoe UI", 12F);
-            L_MiddleText.ForeColor = SystemColors.ControlDarkDark;
-            L_MiddleText.Location = new Point(0, 35);
-            L_MiddleText.Name = "L_MiddleText";
-            L_MiddleText.Size = new Size(324, 301);
-            L_MiddleText.TabIndex = 2;
-            L_MiddleText.Text = "label1";
-            L_MiddleText.TextAlign = ContentAlignment.MiddleCenter;
+            TP_ProtectedFolder.Controls.Add(button2);
+            TP_ProtectedFolder.Controls.Add(button1);
+            TP_ProtectedFolder.Location = new Point(4, 24);
+            TP_ProtectedFolder.Name = "TP_ProtectedFolder";
+            TP_ProtectedFolder.Padding = new Padding(3);
+            TP_ProtectedFolder.Size = new Size(310, 49);
+            TP_ProtectedFolder.TabIndex = 1;
+            TP_ProtectedFolder.Text = "Unlock";
+            TP_ProtectedFolder.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Left;
+            button2.Font = new Font("Segoe UI", 9F);
+            button2.Location = new Point(3, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(78, 43);
+            button2.TabIndex = 3;
+            button2.Text = "Remove Protection";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Right;
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(87, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(220, 43);
+            button1.TabIndex = 2;
+            button1.Text = "Unlock Folder";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(324, 336);
-            Controls.Add(P_ProtectFolder);
+            Controls.Add(P_Controls);
             Controls.Add(L_MiddleText);
             Controls.Add(L_Greet);
             Name = "Form1";
             Text = "PwdFolder";
-            P_ProtectFolder.ResumeLayout(false);
-            P_ProtectFolder.PerformLayout();
+            Load += Form1_Load;
+            P_Controls.ResumeLayout(false);
+            P_Controls.PerformLayout();
+            TC_Buttons.ResumeLayout(false);
+            TP_UnprotectedFolder.ResumeLayout(false);
+            TP_ProtectedFolder.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Label L_Greet;
-        private Panel P_ProtectFolder;
-        private MaskedTextBox TB_Password;
-        private Button B_ProtectFolder;
         private Label L_MiddleText;
+        private MaskedTextBox TB_Password;
+        private Panel P_Controls;
+        private TabControl TC_Buttons;
+        private TabPage TP_UnprotectedFolder;
+        private TabPage TP_ProtectedFolder;
+        private Button B_ProtectFolder;
+        private Button button2;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using Transitions;
+
 namespace PwdFolder
 {
     public partial class Form1 : Form
@@ -5,6 +7,28 @@ namespace PwdFolder
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static void HideAllTabsOnTabControl(TabControl tabControl)
+        {
+            tabControl.Appearance = TabAppearance.FlatButtons;
+            tabControl.ItemSize = new Size(0, 1);
+            tabControl.SizeMode = TabSizeMode.Fixed;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HideAllTabsOnTabControl(TC_Buttons);
+
+            //Set the height of the controls so that tabs are hidden when launched and visible on designer
+            P_Controls.Height = 90;
+            TC_Buttons.Height = 10;
+        }
+
+        private void AnimateMiddleTextUpwards()
+        {
+            L_MiddleText.Height = 295;
+            Transition.run(L_MiddleText, "Height", 205, new TransitionType_EaseInEaseOut(250));
         }
     }
 }
